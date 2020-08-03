56aebea5164d1a58a77267ee9c6d1800f9b7f425
Guangyu <li@semcog.org>
2020-08-03 13:10:40 -0400

    resolve balancing error, update region yaml, add generic settings and tract00 to puma

--- notebooks/popsim_input_maker.py
+++ notebooks/popsim_input_maker.py
@@ -3,40 +3,48 @@
 
 
 # %%
-### prepare all inputs files needed by PopulationSim(RSG)
-## popsim inputs: geo_crosswalk, Census marginals and PUMS samples
-## this programs runs with region.yml(as config), and input_utils.py
-## this programs also needs controls_pre.csv and ACS sample HHs and persons
-## > python popsim_input_maker.py key yml
+## This program prepares all inputs files needed by PopulationSim(RSG), including configuration, geo_crosswalk, Census marginals and PUMS sample households and persons
+## Syntax:  > python popsim_input_maker.py key yaml (key: Census API key; yaml: a yaml configuration file such as region.yaml)
+## Inputs (defined in yaml config):
+##      preprocess/semcog_res_setting.yaml   (a generic setting file for populationsim)
+##      preprocess/control_pre.csv   (control file same as populationsim, with additional "acs_variables" column)
+##      preprocess/ACS/xxxxhxx.csv   (PUMS household samples)
+##      preprocess/ACS/xxxxpxx.csv   (PUMS person samples)
+##      preprocess/ACS/tract0010_to_puma.csv   (tract00 and tract10 to PUMA crosswalk file)
+
 
 # %%
 import os, re, time
 import pandas as pd
 from census import Census
-import yaml
+import oyaml as yaml
 from collections import defaultdict
 from input_utils import *
 import argparse
+import shutil
 
+# %%
 parser = argparse.ArgumentParser()
 parser.add_argument("key", help="Census API key")
-parser.add_argument("yml", help="yaml configuration file name")
+parser.add_argument("yaml", help="yaml configuration file name")
 args = parser.parse_args()
 t0 = time.time()
 
 # %%
-conf = yaml.load(open("./" + args.yml, "r"), Loader=yaml.Loader)
+conf = yaml.load(open("./" + args.yaml, "r"), Loader=yaml.Loader)
 
 
 # %%
 acs_year = conf["year"]
-print("synthersizing year ", acs_year)
+print("\nsynthersizing population for year ", acs_year)
 
 c = Census(args.key, year=acs_year).acs5  # define dataset
 prj_name = conf["region"]["name"]
+data_folder = conf["data_folder"]
 state = conf["region"]["state"][0]
 counties = conf["region"]["counties"]
 pre_folder = conf["preprocess"]["folder"]
+settings_file = "../" + pre_folder + conf["preprocess"]["settings_file"]
 h_pums_csv = (
     "../" + pre_folder + conf["preprocess"]["h_pums_csv"].format(str(acs_year - 2000))
 )
@@ -44,13 +52,15 @@ p_pums_csv = (
     "../" + pre_folder + conf["preprocess"]["p_pums_csv"].format(str(acs_year - 2000))
 )
 
-ouptut_geo_cross = "../{}{}_geo_cross_walk.csv".format(pre_folder, prj_name)
-output_control = "../{}{}_control_totals_.csv".format(pre_folder, prj_name)
-output_seed_hhs = "../{}{}_seed_households.csv".format(pre_folder, prj_name)
-output_seed_persons = "../{}{}_seed_persons.csv".format(pre_folder, prj_name)
+output_folder = "../{}/{}/".format(data_folder, str(acs_year))
+output_geo_cross = "{}_{}_geo_cross_walk.csv".format(prj_name, str(acs_year))
+output_control = "{}_{}_control_totals_.csv".format(prj_name, str(acs_year))
+output_seed_hhs = "{}_{}_seed_households.csv".format(prj_name, str(acs_year))
+output_seed_persons = "{}_{}_seed_persons.csv".format(prj_name, str(acs_year))
+
 
 # %% [markdown]
-# # make geographic cross work file
+# step 1. make geographic cross work file
 
 # %%
 print(
@@ -67,18 +77,18 @@ df_geo["blkgrpid"] = df_geo["tractid"] + df_geo["block group"]
 df_geo.columns = [col.upper() for col in df_geo.columns]
 
 # %%
+# depends on synthesizing year, switch to different tract-PUMA files
 if acs_year >= 2017:
-
     df_tract_puma = pd.read_csv(conf["geographies"]["tract_puma_file"], dtype=str)
     df_tract_puma.rename(columns={"PUMA5CE": "PUMA"}, inplace=True)
 elif (acs_year >= 2010) & (acs_year <= 2016):
     df_tract_puma = pd.read_csv(
-        "../" + conf["geographies"]["tract_puma0010_file"], dtype=str
+        "../" + pre_folder + conf["geographies"]["tract_puma0010_file"], dtype=str
     )
     df_tract_puma.columns = [col.upper() for col in df_tract_puma.columns]
     if acs_year >= 2012:
         df_tract_puma.fillna("00000", inplace=True)
-        df_tract_puma["PUMA"] = df_tract_puma["PUMA00_ID"] + df_tract_puma["PUMA10_ID"]
+        df_tract_puma["PUMA"] = df_tract_puma["PUMA10_ID"] + df_tract_puma["PUMA00_ID"]
     else:
         df_tract_puma.rename(columns={"PUMA00_ID": "PUMA"}, inplace=True)
 else:
@@ -88,23 +98,25 @@ else:
 df_tract_puma = df_tract_puma.loc[df_tract_puma.STATEFP == acgeo.states]
 df_tract_puma["TRACTID"] = (
     df_tract_puma["STATEFP"] + df_tract_puma["COUNTYFP"] + df_tract_puma["TRACTCE"]
-)
+)  # remove  for now to reduce ID length as very long IDs cause calculation errors in populationsim
 
 
 # %%
+# join tract-PUMA and creat geo cross file
 df_geo_cross = pd.merge(df_geo, df_tract_puma, on="TRACTID", how="left")
 df_geo_cross["REGION"] = 1
-print("  saving geo cross walk file: " + ouptut_geo_cross)
-df_geo_cross.to_csv(ouptut_geo_cross)
+df_geo_cross = df_geo_cross[["TRACTID", "BLKGRPID", "PUMA", "REGION"]]
+print("  saving geo cross walk file: " + output_folder + output_geo_cross)
+df_geo_cross.to_csv(output_folder + output_geo_cross)
 
 
 # %% [markdown]
-# # make maginal control files
+# step 2. download and compile maginal control files
 
 # %%
-# download all needed Census marginal controls
-# Census marginal controls variables are from controls_pre table (popsim "controls" table + "acs_variables" field )
-# "acs_variables" contains Census API variables and expressions
+# download Census marginal controls
+# Census controls variables are defined in "controls_pre" table(popsim "controls" table + "acs_variables" field )
+# "acs_variables" contains Census variables and expressions
 #
 print("\nMaking popsim controls")
 print("  downloading Census variables ...")
@@ -140,34 +152,31 @@ for geo, dfg in dfc.groupby("geography"):
 
 
 # %%
-# add unique geoids and PUMA
+# add unique geoids
+ctr_geos = {}
 for geo, dfm in dic_margs.items():
 
     # dfcross = pd.read_csv(geo_cross_csv, dtype = str)
     if dfm.index.nlevels == 3:
-        dfm["TRACTID"] = [l1 + l2 + l3 for l1, l2, l3 in dfm.index]
-        df_geo_tract = df_geo_cross.drop_duplicates("TRACTID")
-        dfm = pd.merge(
-            dfm.reset_index(),
-            df_geo_tract[["TRACTID", "PUMA"]],
-            on="TRACTID",
-            how="left",
-        )
+        dfm["TRACTID"] = [l1 + l2 + l3 for l1, l2, l3 in dfm.index]  # remove l1 for now
     elif dfm.index.nlevels == 4:
-        dfm["BLKGRPID"] = [l1 + l2 + l3 + l4 for l1, l2, l3, l4 in dfm.index]
-        dfm = pd.merge(
-            dfm.reset_index(),
-            df_geo_cross[["BLKGRPID", "PUMA"]],
-            on="BLKGRPID",
-            how="left",
-        )
+        dfm["BLKGRPID"] = [
+            l1 + l2 + l3 + l4 for l1, l2, l3, l4 in dfm.index
+        ]  # remove l1 for now
+    dfm.reset_index(drop=True, inplace=True)
+
+    dfm.fillna(0, inplace=True)
     dfm.columns = [col.upper() for col in dfm.columns]
-    output_control = output_control.replace(".csv", geo.lower() + ".csv")
-    print("  saving control file: " + output_control)
-    dfm.to_csv(output_control)
+    if "HHBASE" in dfm.columns:
+        dfm = dfm.loc[dfm.HHBASE > 0]
+
+    f_output_control = output_control.replace(".csv", geo.lower() + ".csv")
+    ctr_geos[geo] = f_output_control
+    print("  saving control file: " + output_folder + f_output_control)
+    dfm.to_csv(output_folder + f_output_control)
 
 # %% [markdown]
-# # extract PUMS seed households and persons
+# step 3. extract PUMS seed households and persons
 
 print("\nExtrating PUMS seed households and persons")
 
@@ -183,14 +192,14 @@ h_pums = pd.read_csv(h_pums_csv, index_col="SERIALNO")
 p_pums = pd.read_csv(p_pums_csv)
 emp_df = pd.DataFrame()
 h_samples, p_samples = [], []
-
+count = 0
 
 if (acs_year <= 2011) or (acs_year >= 2017):
     h_pums = h_pums.loc[h_pums.PUMA.isin(puma_lst)]
     p_pums = p_pums.loc[p_pums.PUMA.isin(puma_lst)]
 else:
     pums_grp = {}
-    for pma in ["PUMA00", "PUMA10"]:
+    for pma in ["PUMA10", "PUMA00"]:
         pums_grp[pma] = defaultdict(dict)
         for indx, grp in h_pums.loc[h_pums[pma] != -9].groupby(pma):
             pums_grp[pma]["households"][indx] = grp
@@ -200,21 +209,24 @@ else:
         pums_grp[pma]["persons"][0] = emp_df
 
     for puma in puma_lst:
+        count += 1
         h_puma = pd.concat(
             [
-                pums_grp["PUMA00"]["households"][int(puma[:5])],
-                pums_grp["PUMA10"]["households"][int(puma[5:])],
+                pums_grp["PUMA10"]["households"][int(puma[:5])],
+                pums_grp["PUMA00"]["households"][int(puma[5:])],
             ]
         )
         h_puma["PUMA"] = puma
+        h_puma.index = count * (10 ** 13) + h_puma.index
         h_samples.append(h_puma)
         p_puma = pd.concat(
             [
-                pums_grp["PUMA00"]["persons"][int(puma[:5])],
-                pums_grp["PUMA10"]["persons"][int(puma[5:])],
+                pums_grp["PUMA10"]["persons"][int(puma[:5])],
+                pums_grp["PUMA00"]["persons"][int(puma[5:])],
             ]
         )
         p_puma["PUMA"] = puma
+        p_puma["SERIALNO"] = count * (10 ** 13) + p_puma["SERIALNO"]
         p_samples.append(p_puma)
 
     h_pums = pd.concat(h_samples)
@@ -225,17 +237,118 @@ h_pums = h_pums.loc[
 ]  # remove group quarters and empty units
 p_pums = p_pums.loc[p_pums["SERIALNO"].isin(h_pums.index)]
 
+h_pums, p_pums = preprocess_pums(h_pums, p_pums)
+
+h_pums["hh_id"] = h_pums.index.values
+p_pums["hh_id"] = p_pums.SERIALNO
+
 print(
     "  saving seed households: {}   . {} records".format(
-        output_seed_hhs, str(len(h_pums))
+        output_folder + output_seed_hhs, str(len(h_pums))
     )
 )
-h_pums.to_csv(output_seed_hhs)
+h_pums.to_csv(output_folder + output_seed_hhs)
 print(
     "  saving seed persons: {}   . {} records".format(
-        output_seed_persons, str(len(p_pums))
+        output_folder + output_seed_persons, str(len(p_pums))
     )
 )
-p_pums.to_csv(output_seed_persons)
+p_pums.to_csv(output_folder + output_seed_persons)
 
-print("Done.", "\ntotal time: {} seconds".format(round(time.time() - t0, 1)))
+
+# %% [markdown]
+# step 4. modify popsim config
+print("\nupdate popsim settings")
+
+# %%
+SORT_ORDER = [
+    "REGION",
+    "PUMA",
+    "MCD",
+    "SAMPLEGEO",
+    "TRACT",
+    "BLKGRP",
+    "TAZ",
+    "BUILDING",
+]
+sorted_geos = list(ctr_geos.keys())
+sorted_geos.sort(key=lambda val: SORT_ORDER.index(val))
+
+
+# %%
+prj_config = yaml.load(open(settings_file, "r"), Loader=yaml.FullLoader)
+
+# %%
+geos = sorted_geos + ["REGION", "PUMA"]
+geos.sort(key=lambda val: SORT_ORDER.index(val))
+prj_config["geographies"] = geos  # sort by predefined order)
+prj_config["seed_geography"] = "PUMA"
+
+prj_config["data_dir"] = "data/" + str(acs_year)
+
+# %%
+for litem in prj_config["input_table_list"]:
+    if litem["tablename"] == "households":
+        litem["filename"] = output_seed_hhs
+    if litem["tablename"] == "persons":
+        litem["filename"] = output_seed_persons
+    if litem["tablename"] == "geo_cross_walk":
+        litem["filename"] = output_geo_cross
+    if "_control_data" in litem["tablename"]:
+        geo = litem["tablename"].replace("_control_data", "")
+        if geo not in ctr_geos.keys():
+            prj_config["input_table_list"].remove(litem)
+        else:
+            litem["filename"] = ctr_geos[geo]
+            del ctr_geos[geo]
+for k in ctr_geos:
+    prj_config["input_table_list"].append(
+        {"tablename": k + "_control_data", "filename": ctr_geos[k]}
+    )
+
+# %%
+prj_config["control_file_name"] = "{}_{}_controls.csv".format(prj_name, str(acs_year))
+prj_config["output_tables"] = {
+    "action": "include",
+    "tables": ["summary_" + x for x in sorted_geos],
+}
+
+
+sub_bal_lst = ["sub_balancing.geography=" + x for x in sorted_geos]
+prj_config["run_list"]["steps"] = (
+    [
+        "input_pre_processor",
+        "setup_data_structures",
+        "initial_seed_balancing",
+        "meta_control_factoring",
+        "final_seed_balancing",
+        "integerize_final_seed_weights",
+    ]
+    + sub_bal_lst
+    + ["expand_households", "summarize", "write_tables", "write_synthetic_population"]
+)
+
+with open(
+    "{}{}_{}_settings.yaml".format(output_folder, prj_name, acs_year), "w"
+) as yaml_file:
+    yaml.dump(prj_config, yaml_file, default_style=None, default_flow_style=False)
+
+
+# %% [markdown]
+# # copy pre control file
+
+print("copy popsim master control file")
+shutil.copy(
+    "../" + pre_folder + conf["preprocess"]["pre_control"],
+    output_folder + "{}_{}_controls.csv".format(prj_name, str(acs_year)),
+)
+
+
+# %% [markdown]
+
+print(
+    "\ntotal time: {} seconds".format(round(time.time() - t0, 1)),
+    "\nDone. All files are saved to " + output_folder,
+    '\nTo run Populationsim, copy new settings and controls to configs folder and rename "xxx_settings.yaml" to "settings.yaml"',
+)
+
